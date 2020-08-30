1eb8e8120879888a8923c4abfd0bb82fb726e0a0
sammyonyekwere <saamzinc@gmail.com>
2020-08-30 11:07:10 +0100

    Learnt about integers and floats

--- fundamentals.go
+++ fundamentals.go
@@ -15,4 +15,15 @@ func main() {
 	// This will return true since a is equal to c
 	fmt.Println(b == c)
 
+	// Intrgers and Floating point numbers
+	// Integers do not have decimal paces
+	// Floating point numbers have decimal places
+	// Intergers are denoted generally by int
+	// While floating point numbers are denoted by float64
+	var x int = 64
+	var y float64 = 33.3
+
+	fmt.Println(x)
+	fmt.Println(y)
+
 }
